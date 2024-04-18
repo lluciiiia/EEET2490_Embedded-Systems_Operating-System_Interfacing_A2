4338ef6ae601277113504e7f6389710fcb9d733f
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-04-18 15:08:28 +0700

    fix: setcolor command read

--- Q1/header/command.h
+++ Q1/header/command.h
@@ -1,11 +1,11 @@
 #include "../header/uart0.h"
 #include "../header/util.h"
 
-void help_command();
+void help_command(char *command);
 
 void clear_command();
 
-void setcolor_command(char *text_color, char *background_color);
+void setcolor_command(char *command);
 
 void showinfo_command();
 
