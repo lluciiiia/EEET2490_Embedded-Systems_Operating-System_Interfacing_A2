8e6bb0dade855a5ecfbc828a358685d0ee6fa603
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-04-27 14:26:01 +0700

    fix: dec -> hex to print the baudrate

--- peripheral/uart0.c
+++ peripheral/uart0.c
@@ -224,9 +224,9 @@ void set_baud_rate_command(char *arg)
 	}
 
 	uart_puts("\nIBRD before setting: ");
-	uart_dec(UART0_IBRD);
+	uart_hex(UART0_IBRD);
 	uart_puts("\nFBRD before setting: ");
-	uart_dec(UART0_FBRD);
+	uart_hex(UART0_FBRD);
 	uart_puts("\n\n");
 
 	// Calculate the baud rate divisor
@@ -240,11 +240,11 @@ void set_baud_rate_command(char *arg)
 	UART0_CR = UART0_CR_UARTEN | UART0_CR_TXE | UART0_CR_RXE; // Enable UART0, Tx, Rx
 
 	uart_puts("\nIBRD after setting: ");
-	uart_dec(UART0_IBRD);
+	uart_hex(UART0_IBRD);
 	uart_puts("\nFBRD after setting: ");
-	uart_dec(UART0_FBRD);
+	uart_hex(UART0_FBRD);
 	uart_puts("\n\nBaud Rate has been set to ");
-	uart_dec(baud_rate);
+	uart_hex(baud_rate);
 	display_end();
 }
 
